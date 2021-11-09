55975428c391d6536e46da33478c9ab1a0133242
Craig Neff <ifly12610@gmail.com>
2021-11-08 23:59:30 -0700

    more updates to CSS and fixing layout

---
+++ index.js
@@ -0,0 +1,8 @@
+let c = "camelCasing"; //  "camel Casing"
+let j = "identifier"; //  "identifier"
+let h = ""; //  ""
+
+// complete the function
+function kickAss() {
+  console.log(`You pressed a button`);
+}
